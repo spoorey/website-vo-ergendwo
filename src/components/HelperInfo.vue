a2f106793f06ae902c094e3e48542ff88cce78b9
David Spoerri <spoerridavid@gmail.com>
2019-10-02 13:28:41 +0200

    Add philosophy text

--- src/components/HelperInfo.vue
+++ src/components/HelperInfo.vue
@@ -5,11 +5,50 @@
     <h2 class="text-center">Mitmachen</h2>
     <p class="lead text-center"><strong>Du willst Geflüchteten helfen?</strong><strong class="text-danger"> Wir suchen dich!</strong></p>
     <!--<img src="../assets/helper-1.jpeg" class="img-fluid float-left d-inline d-lg-none">-->
-    <p class="lead text-center">
+    <div class="lead text-center">
+      <p>
       Wenn Du uns unterstützen möchtest, gibt es verschiedene Möglichkeiten. Eine grosse Hilfe ist es, wenn Du unsere Aktivitäten aktiv verfolgst, an unseren Anlässen teilnimmst oder vielen Menschen von unserem Angebot erzählst.
-      <br>
+      </p><p>
       Falls Dir das zu wenig ist, kannst Du auch Helfer*in oder Gönner*in Vo Ergendwo werden.
-    </p>
+      </p>
+      <div>
+        <b-button v-b-toggle.collapse-philosophy variant="primary">
+          <i class="fa fa-plus when-closed"></i>
+          <i class="fa fa-minus when-opened"></i>
+          Unsere Philosophie
+        </b-button>
+        <b-collapse id="collapse-philosophy" class="mt-2">
+          <b-card>
+            <div class="card-text text-left">
+              <p>
+                Die grosse Flüchtlingsbewegungen der Jahre 2015-2017 in Europa führten zu ungewohnten Herausforderungen.
+                Es war klar, dass es verschiedenste Meinungen und Ideen gab, wie die Gesellschaft damit umgehen sollte.
+                Oftmals kamen Forderungen auf, dass entweder diese oderjene sich besser integrieren sollten oder der Staat die Integration mehr unterstützen müsse.
+                Es gab an allen Ecken viel Verbesserungspotential.
+                Im Zentrum aller Aufmerksamkeit stand aber immer der grosse Begriff “Integration”.
+              </p>
+              <p>
+                Wir vom Verein «Vo Ergendwo» bekamen diese Diskussionen natürlich auch mit.
+                Uns interessierte aber weniger der Begriff Integration an sich, sondern eigentlich ganz andere Dinge.
+                Wir fragten uns, wie man geflüchteten Menschen in der Schweiz helfen kann und welche unterschiedlichen Menschen nun unsere neuen Mitbürgerinnen und Mitbürger wurden.
+                Voller Neugier und Tatendrang startete dann dieses Projekt, welches ziemlich schnell ein klares Konzept entwickelte, das in dieser Form bis anhin in keinen anderen Integrationsprojekten umgesetzt wurde.
+                Der Deutschtreff soll genau diese Neugier und Entdeckerlust wecken, Menschen zusammenbringen und Dialoge ermöglichen.
+              </p>
+              <p>
+                Wir möchten nicht, dass es bei Integration vor allem darum geht, wer zu wenig macht.
+                Wir möchten nicht, dass das Hauptaugenmerk darauf liegt, welche Geflüchteten zu wenig Deutschlernen oder sich zu wenig anpassen.
+                Wir möchten uns aber auch nicht auf den Bund verlassen müssen, wenn es darum geht, eine vielseitige Gesellschaft und ein gutes Miteinander zu ermöglichen.
+                Wir sind vielmehr überzeugt, dass jede_r von uns seinen Teil dazu beitragen kann, eine heterogene und lebendige Gesellschaft zu sein.
+                Zu einer solchen Gesellschaft gehört unserer Meinung nach auch, dass neue Einflüsse, Ideen, Kulturen und Menschen darin aufgenommen werden können.
+                Im Endeffekt geht es uns also um das Miteinander. Es ist unsere Aufgabe, mit unseren Mitmenschen auszukommen, also schauen wir doch, dass wir diese Mitmenschen kennenlernen, Barrieren brechen und zusammen weiter kommen.
+                Auf dieser Überzeugung ist auch der Deutschtreff aufgebaut. Unser Deutschtreff soll in erster Linie ein Ort sein, an dem Leute zusammenkommen. Wie unser Name „Vo Ergendwo“ bereits verrät, spielt es dabei keine Rolle, woher jemand kommt, sondern es reicht die Freude darüber, dass man sich kennen lernt.
+                Unser Deutschtreff soll alles sein können und nichts sein müssen. Gemeinsam lernen wir Deutsch, spielen Fussball, hüten Kinder, plaudern über den Alltag, essen unbekannte Mahlzeiten, gehen auf Ausflüge usw. Man hilft sich gegenseitig und alle profitieren davon. Auch im alltäglichen Leben lässt sich das umsetzen, im Bewusstsein, dass Integration weniger diskutiert, sondern einfach mehr gelebt werden muss.
+              </p>
+            </div>
+          </b-card>
+        </b-collapse>
+      </div>
+    </div>
     <div>
       <h3>Helfer*in Vo Ergendwo</h3>
       <img src="../assets/helper.jpg" class="img img-fluid rounded m-2 d-none d-lg-block" style="max-width: 33%; float: left;">
@@ -44,7 +83,6 @@
         <img src="../assets/benefactor.jpg" class="img img-fluid rounded m-2 d-none d-lg-block" style="max-width: 33%; float: right;">
         <img src="../assets/benefactor.jpg" class="img img-fluid rounded m-2 d-none d-md-block d-lg-none" style="max-width: 50%; float: right;">
         <img src="../assets/benefactor.jpg" class="img img-fluid rounded d-block d-md-none" style="float: right;">
-        <br>
         Natürlich kann man uns auch gerne finanziell unterstützen.<br>
         Die Möglichkeit, Gönner*in Vo Ergendwo zu werden, ist gemacht für Menschen, die sich gerne für andere einsetzen, um ihnen einen leichteren Start in der Schweiz zu ermöglichen, gleichzeitig aber keine Zeit haben, sich aktiv am Deutschtreff zu beteiligen.
         <br>
@@ -74,8 +112,13 @@ section p {
   overflow: hidden;
 }
 
-
 a.btn {
   margin-bottom: 5px;
 }
+
+.collapsed > .when-opened,
+:not(.collapsed) > .when-closed {
+  display: none;
+}
+
 </style>
