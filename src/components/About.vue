f3656a1ad150de485e0c02cff734bd167dba5743
David Spoerri <spoerridavid@gmail.com>
2019-10-09 14:55:20 +0200

    Add pictures

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,6 +3,7 @@
     <span id="about" class="nav-anchor"></span>
     <div class="about">
         <h1> {{ t('about_slogan') }}</h1>
+        <img class="pencil" src="../assets/pencil.png">
         <p>{{ t('about_lead') }}</p>
         <a class="btn btn-ve btn-lg" href="#location" v-scroll-to="'#location'" >
           <i class="fa fa-route"></i> 
@@ -20,17 +21,30 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style lang="less">
 .about {
   text-align: center;
   padding: 5%;
   display: block;
-  background-image: url("../assets/deutschtreff-2.jpg");
+  background-image: url("../assets/about.jpg");
   background-repeat: no-repeat;
   background-position: center;
   background-attachment: fixed;
   background-size: cover;
   color: #222;
 
+  h1 {
+    line-height: normal;
+    margin-bottom: 2px;
+  }
+
+  img.pencil  {
+    max-height: 30px;
+    max-width: 50%;
+  }
+
+  p {
+    font-size: 1.1rem;
+  }
 }
 </style>
