b9d069dad9632ef8fa3554bf42fc6e2ac77563a1
David Spörri <spoerridavid@gmail.com>
2020-02-16 21:46:09 +0100

    update fest content

--- src/components/Party.vue
+++ src/components/Party.vue
@@ -6,30 +6,66 @@
       <h2 class="text-center">{{ t('Jubiläumsfest am 22. Februar') }}</h2>
 
       <b-row>
-        <b-col cols="12" sm="6" lg="4" class="text-center">
-          <img src="../assets/fest-1.png" class="img img-fluid p-0" />
+        <b-col cols="12" sm="4" class="p-0 text-center">
+          <img src="../assets/fest-poster.png" class="img img-fluid d-inline-block" /><br><br>
+          <img src="../assets/fest-1.png" class="img img-fluid d-none d-sm-inline-block">
         </b-col>
+        <b-col cols="12" sm="8" class="text-center">
+          Liebe Freunde des Vereins Vo Ergendwo<br>
 
-        <b-col cols="12" sm="6" lg="4" class="text-center party-text">
-          <strong>
-            {{ t('Feier am 22. Februar mit uns das 4-Jährige Jubiläum des Deutschtreffs') }}
-          </strong>
+          Wir laden herzlich zu unserem 4-Jahres-Jubiläum! Es erfüllt uns mit Stolz und Freude, dass es uns schon so lange und erfolgreich gibt &lt;3<br>
+          Und das wollen wir gerne mit unseren Freunden aus aller Welt, mit unseren Teilnehmenden, Helfer*innen und Gönner*innen feiern =)<br>
           <br>
+          <h2>Programm und Zeiten</h2>
+          Unser diesjähriges Jubiläumsfest findet am <em>Samstag, 22. Februar</em> statt.<br>
+          Wir bieten euch wie immer ein vielseitiges Programm:<br><br>
+          <table class="table">
+              <tr>
+                  <td><strong>17:00 Uhr</strong></td>
+                  <td>Eintrudeln</td>
+              </tr>
+
+              <tr>
+                  <td><strong>18:00 - 20:00 Uhr</strong></td>
+                  <td> internationales Buffet</td>
+              </tr>
+
+              <tr>
+                  <td><strong>19:00 - 21:00 Uhr</strong></td>
+                  <td> Open Stage</td>
+              </tr>
+
+              <tr>
+                  <td><strong>21:30 - 23:00 Uhr</strong></td>
+                  <td> Band NOZEZ (Gypsy / Balkan)</td>
+              </tr>
+
+              <tr>
+                  <td><strong>23:00 - 02:00 Uhr</strong></td>
+                  <td> DJ Sonosphere (orientalisch, Techno)</td>
+              </tr>
+          </table>
+          <strong>Internationales Buffet</strong><br>
+          Wie gewohnt kochen wir euch mit den Teilnehmenden zusammen leckere Speisen aus verschiedenen Ländern - immer ein grossartiger Schmaus, den man nicht verpassen sollte ;-)<br>
           <br>
-            {{ t('Neben Essen aus aller Welt erwarten dich eine Open Stage, Bands und DJs.') }}
+          <strong>Open Stage</strong><br>
+          Die Open Stage lädt ein, eine eigene kleine Darbietung zum besten zu geben. Einzelne Deutschtreff-Teilnehmer*innen werden singen und musizieren und man darf spontan einen Teil dazu beitragen. Instrumente und Mikrofone sind vorhanden - Wir freuen uns wenn du mitmachst!
+          <h2>Ort</h2>
+          <b>Kulturhaus Tommasini</b><br>
+          Seonerstrasse 23<br>
+          5600 Lenzburg<br>
+          <a class="btn btn-ve btn-sm" href="#location" v-scroll-to="'#location'">Anreise</a>
+          <hr>
+          <strong>Eintritt:</strong>
+          Kollekte
           <br>
+          Bitte diese Veranstaltung teilen und weitersagen! Wir freuen uns schon riesig auf euch und auf ein weiteres, unvergessliches Fest &lt;3
           <br>
-          <strong>{{ t('Ort') }}:</strong> Tommasini Lenzburg
-          <a class="btn btn-ve btn-sm" href="#location" v-scroll-to="'#location'">{{ t('Anreise') }}</a>
+          Euer Verein Vo Ergendwo
           <br>
-          <strong>{{ t('Zeit') }}:</strong> {{ t('22. Februar 2020 ab 18 Uhr') }}
-          <br>
-          <br>
-          <span class="text-ve">{{ t('Weitere Informationen folgen bald') }}</span>
-        </b-col>
-        
-        <b-col cols="4" class="text-center">
-          <img src="../assets/fest-2.png" class="img img-fluid p-0 d-none d-lg-block" />
+          <a href="https://www.facebook.com/events/542108523319764/" class="text-ve">
+            <i class="fab fa-facebook"></i>
+            Facebook-Event</a>
         </b-col>
       </b-row>
     </div>
@@ -53,5 +89,12 @@ img {
 
 .party-text {
   font-size: 18pt;
+}
+table tr td:nth-child(2n) {
+  text-align: left;
+}
+
+table tr td:nth-child(2n-1) {
+  text-align: right;
 }
 </style>
