138070119d3a5e4d267059096d0e187ff0421146
David Spörri <spoerridavid@gmail.com>
2020-05-26 21:09:16 +0200

    update corona info

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,13 +5,36 @@
         <h1> {{ t('Wir sind alle \"Vo Ergendwo\"') }}</h1>
         <img class="pencil" src="../assets/pencil.png">
         <p>{{ t('Deutschtreff für Geflüchtete - jeden Dienstag 18:00 bis 20:00 im Tommasini Lenzburg') }}</p>
-        <p class="corona alert alert-danger">
-          Liebe Teilnehmende des Deutschtreffs.<br>
-          Leider müssen wir den Deutschtreff bis auf weiteres schliessen.<br>
-          Grund dafür ist der Coronavirus-Ausbruch.<br>
-          Wir werden euch informieren, wenn wir den Deutschtreff wieder öffnen können.<br>
-          Falls ihr dringend Hilfe von uns braucht, könnt ihr uns trotzdem kontaktieren.
-        </p>
+        <div class="alert alert-danger">
+          <div v-if="this.getLang == 'de'">
+            Liebe Teilnehmende des Deutschtreffs.<br>
+            Leider müssen wir den Deutschtreff bis auf weiteres schliessen.<br>
+            Grund dafür ist der Coronavirus-Ausbruch.<br>
+            Wir werden euch informieren, wenn wir den Deutschtreff wieder öffnen können.<br>
+            Falls ihr dringend Hilfe von uns braucht, könnt ihr uns trotzdem kontaktieren.
+            <br>
+            <strong>Stand 20.05.2020</strong><br>
+            Wir warten momentan auf genauere Informationen vom Bundesrat und der Regierung der Stadt Lenzburg.<br>
+            Wir hoffen, dass der Deutschtreff bis mitte Juni wieder geöffnet werden kann.<br>
+            Im Moment wissen wir aber noch nicht genau, wann und wie wir den Deutschtreff öffnen können.<br>
+            <br>
+            Auf der Website des BAG findet ihr Informationen zum Coronavirus in verschiedenen Sprachen (unter anderem Farsi, Tigrinisch, Tibetisch und viele mehr):<br>
+            <a href="https://bag-coronavirus.ch/downloads/">bag-coronavirus.ch/downloads/</a>
+          </div>
+          <div v-if="this.getLang != 'de'">
+            Dear participants of the language meet-up.<br>
+            Regrettably, we have to close the language meetup for now, due to the coronavirs pandemic.<br>
+            We will inform you, as soon as we can open the language meet-up again.<br>
+            If you need help during this time, feel free to contact us.
+            <br>
+            <strong>Update 20.05.2020</strong><br>
+            We are currently waiting for information by the federal council (Bundesrat) and the government of the town of lenzburg.<br>
+            We hope to be able to open the language meet-up in mid june. Right now however, we don't know exactly when and how we can open up the language meet-up again.<br>
+            <br>
+            On the BAG website, you can find information on the coronavirus in various languages, (including Farsi, Tigrinya, Tibetan and many more):<br>
+            <a href="https://bag-coronavirus.ch/downloads/">bag-coronavirus.ch/downloads/</a>
+          </div>
+        </div>
         <a class="btn btn-ve btn-lg" href="#contact" v-scroll-to="'#contact'" >
           <i class="fa fa-phone"></i> 
           {{ t('Kontakt') }}
@@ -29,6 +52,11 @@
 <script>
 export default {
   name: 'about',
+  computed: {
+    getLang: function() {
+      return this.$translate.lang;
+    }
+  },
   data () {
     return {}
   }
@@ -59,10 +87,6 @@ export default {
   p {
     font-size: 1.1rem;
     text-decoration: line-through;
-  }
-
-  p.corona {
-    text-decoration: none;
   }
 }
 </style>
