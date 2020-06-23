643de3c8c5edbd6bb31f461b66a31d3ad25afcbf
David Spörri <spoerridavid@gmail.com>
2020-06-23 14:15:26 +0200

    update corona and summer info

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,19 +8,25 @@
         <div class="alert alert-success">
           <div v-if="this.getLang == 'de'">
             Liebe Teilnehmende des Deutschtreffs.<br>
-            Wir können ab dem 16. Juni wieder Deutschtreff durchführen.<br>
+            Wir werden vom 16. Juni - 7. Juli Deutschtreff durchführen. Danach werden wir wie jedes Jahr eine Sommerpause machen.<br>
             Wir werden auf gute Hygiene achten und auch eine Anwesenheitsliste führen, um im Falle einer Infektion Infektionsketten unterbrechen zu können.<br> 
+            Wir freuen uns, euch wieder zu sehen!<br>
+            <br>
+            Ihr könnt uns, auch während der Sommerpause, jederzeit kontaktieren, wenn ihr Fragen habt.
             <br>
             Auf der Website des BAG findet ihr Informationen zum Coronavirus in verschiedenen Sprachen (unter anderem Farsi, Tigrinisch, Tibetisch und viele mehr):<br>
             <a href="https://bag-coronavirus.ch/downloads/">bag-coronavirus.ch/downloads/</a>
           </div>
           <div v-if="this.getLang != 'de'">
             Dear participants of the language meet-up.<br>
-            Starting on june 16th, we can conduct the language meeet-up again.<br>
+            We will conduct the language meetup again, from the 16th of June to the 7th of July. After that there will be a summer break, just like every year.<br>
             We will mantain good hygiene and also keep an attendance list so that we can interrupt chains of infection in case of an infection.<br>
+            We're looking forward to seeing you again!<br>
             <br>
             On the BAG website, you can find information on the coronavirus in various languages, (including Farsi, Tigrinya, Tibetan and many more):<br>
             <a href="https://bag-coronavirus.ch/downloads/">bag-coronavirus.ch/downloads/</a>
+            <br>
+            You can always contact us with any questions, even during the summer break.
           </div>
         </div>
         <a class="btn btn-ve btn-lg" href="#contact" v-scroll-to="'#contact'" >
